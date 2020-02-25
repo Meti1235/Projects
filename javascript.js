382bc24fb4cfe7a6115ccc5c864f1aeab8b20cf7
Meti1235 <61099938+Meti1235@users.noreply.github.com>
2020-02-25 18:18:29 +0100

    update

---
+++ javascript.js
@@ -0,0 +1,27 @@
+// let navService = {
+//     navItems: document.getElementsByClassName("nav-item"),
+//     pages: document.getElementById("pages").children,
+
+//     activeNav: function (item) {
+//         for (let navItem of this.navItems) {
+//             navItem.classList.remove("active");
+//         }
+//         item.classList.add("active");
+//     },
+//     showPages: function (page) {
+//         for (let pageElement of this.pages) {
+//             pageElement.style.display = "none";
+//         }
+//         page.style.display = "block";
+//     },
+//     createEventListener: function () {
+//         for (let i = 0; i < this.navItems.length; i++) {
+//             this.navItems[i].addEventListener("click", function () {
+//                 // this in addEventListener points to the item that has the event listener
+//                 navService.activeNav(this);
+//             })
+//         }
+//     }
+
+// }
+console.log("hi")
